331410710bc67840476264a3c3acffec95ff220c
dhirendrabam001 <dhirendrabam12345@gmail.com>
2025-12-18 11:02:21 -0800

    updated all frontend and backend

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOption = {
-  origin: "http://localhost:5173",
+  origin: "https://job-portal-adg1.onrender.com",
   credentials: true,
 };
 app.use(cors(corsOption));
