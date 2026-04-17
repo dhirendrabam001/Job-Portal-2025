8f2f2f6044c4b8504f69832d02b0536ba5a57e7e
Dhirendra Bam <dhirendrabam12345@gmail.com>
2026-04-17 17:06:29 +0545

    fixing length

--- frontend/src/LandingPage/AppliedJobs.jsx
+++ frontend/src/LandingPage/AppliedJobs.jsx
@@ -1,6 +1,8 @@
 import { useSelector } from "react-redux";
+
 const AppliedJobs = () => {
   const { getAppliedJob } = useSelector((store) => store.jobs);
+
   return (
     <>
       <div className="applied-job py-3">
@@ -16,19 +18,21 @@ const AppliedJobs = () => {
             </thead>
 
             <tbody>
-              {getAppliedJob.length <= 0 ? (
+              {(getAppliedJob || []).length <= 0 ? (
                 <tr>
                   <td colSpan="4">You do not have applied any jobs!</td>
                 </tr>
               ) : (
-                getAppliedJob.map((item, index) => {
+                (getAppliedJob || []).map((item, index) => {
                   return (
                     <tr key={index}>
-                      <td>{item?.createdAt.split("T")[0]}</td>
-                      <td>{item?.job?.title}</td>
-                      <td>{item?.job?.company?.companyName}</td>
                       <td>
-                        {item.status === "accepted" ? (
+                        {item?.createdAt ? item.createdAt.split("T")[0] : "N/A"}
+                      </td>
+                      <td>{item?.job?.title || "N/A"}</td>
+                      <td>{item?.job?.company?.companyName || "N/A"}</td>
+                      <td>
+                        {item?.status === "accepted" ? (
                           <span className="status accepted">Accepted</span>
                         ) : item?.status === "rejected" ? (
                           <span className="status rejected">Rejected</span>
