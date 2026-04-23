49e87cc05c7686b5bb85e3d878e9625128d49c9c
Dhirendra Bam <dhirendrabam12345@gmail.com>
2026-04-23 11:01:18 +0545

    updated signup and register

--- frontend/src/Components/Auth/SignUp.jsx
+++ frontend/src/Components/Auth/SignUp.jsx
@@ -1,5 +1,5 @@
 import { Link, useNavigate } from "react-router-dom";
-// import axios from "axios";
+
 import toast from "react-hot-toast";
 import Header from "./Header";
 import { useState } from "react";
